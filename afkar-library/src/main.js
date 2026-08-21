2f83400de721be43c5e8117ad1cc7df0a8a7e1e4
CallMeNephilim <mahdiafkar0@gmail.com>
2026-08-21 20:39:52 +1000

    updating main.js

--- afkar-library/src/main.js
+++ afkar-library/src/main.js
@@ -1,6 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-// import './style.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
 
-createApp(App).mount('#app')
+// Bootstrap & PrimeVue CSS
+import 'bootstrap/dist/css/bootstrap.min.css'
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
+
+const app = createApp(App)
+
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura
+  }
+})
+
+app.mount('#app')
