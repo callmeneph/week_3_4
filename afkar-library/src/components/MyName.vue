6246aae4c0a4a127dfc755b04246bb667e7a4062
CallMeNephilim <mahdiafkar0@gmail.com>
2026-08-21 19:38:47 +1000

    making the repo ready for week 3

---
+++ afkar-library/src/components/MyName.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="my-name">
+    <h2>Hello, my name is Mahdi Afkar! I hope so!</h2>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.my-name {
+  margin: 15px 0;
+  padding: 10px;
+  background-color: #e8f4f8;
+  border-radius: 8px;
+  color: #0c5460;
+}
+</style>
